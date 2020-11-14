cf839ce227db7e1518772f99247dc4c40a9bbf7e
cngzz1 <67855012+cngzz1@users.noreply.github.com>
2020-11-14 08:17:44 +0800

    Update Main.java
    Hi Arthur,
    
    Since this exercise is on polymorphism, instead of
    Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
    
    it should be:
    Car mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
    
    The instructors have acknowledged the error and claimed that it will be fixed in the remaster.
    
    Your classmate,
    Chris

--- src/UdemyJavaProgrammingMasterclass/Section7_CompositionEncapsulationPolymorphism/CarPolymorphism/Main.java
+++ src/UdemyJavaProgrammingMasterclass/Section7_CompositionEncapsulationPolymorphism/CarPolymorphism/Main.java
@@ -7,17 +7,17 @@ public class Main {
         System.out.println(car.accelerate());
         System.out.println(car.brake());
 
-        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
+        Car mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
         System.out.println(mitsubishi.startEngine());
         System.out.println(mitsubishi.accelerate());
         System.out.println(mitsubishi.brake());
 
-        Ford ford = new Ford(6, "Ford Falcon");
+        Car ford = new Ford(6, "Ford Falcon");
         System.out.println(ford.startEngine());
         System.out.println(ford.accelerate());
         System.out.println(ford.brake());
 
-        Holden holden = new Holden(6, "Holden Commodore");
+        Car holden = new Holden(6, "Holden Commodore");
         System.out.println(holden.startEngine());
         System.out.println(holden.accelerate());
         System.out.println(holden.brake());
